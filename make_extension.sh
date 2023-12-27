d1c7f81010aad32831b2b58d5823c87894da821b
Max Dymond <maxmakesmagic@gmail.com>
2023-12-27 20:19:13 +0000

    Remove errant "sha1_urls()" function and add github publishing of extension

--- make_extension.sh
+++ make_extension.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if ! command -v zip &> /dev/null
+then
+    echo "zip could not be found, installing..."
+    apt-get install -y zip
+fi
+
 # Make a zip compatible with Chrome and Firefox
 rm -f deekay.zip
 zip -r deekay.zip assets hashes manifest.json deekay.js
